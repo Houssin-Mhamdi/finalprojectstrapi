015b299f8f2a3fd340ddb1dd3fb41204f8a0da11
houssinCarnelian <houssin.carnelian@gmail.com>
2023-01-20 10:57:26 +0100

    send blue

--- config/plugins.js
+++ config/plugins.js
@@ -15,5 +15,24 @@ module.exports = ({ env }) => ({
     },
   },
   // ...
-  
+  // ...
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST', 'smtp-relay.sendinblue.com'),
+        port: env('SMTP_PORT', 587),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+        // ... any custom nodemailer options
+      },
+      settings: {
+        defaultFrom: 'houssin.carnelian@gmail.com',
+        defaultReplyTo: 'houssin.carnelian@gmail.com',
+      },
+    },
+  },
+  // ...
 });
