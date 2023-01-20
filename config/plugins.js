b7b85efb2870526be34e610460a08755bd81ce6e
houssinCarnelian <houssin.carnelian@gmail.com>
2023-01-20 10:29:36 +0100

    dazdaz

--- config/plugins.js
+++ config/plugins.js
@@ -15,21 +15,5 @@ module.exports = ({ env }) => ({
     },
   },
   // ...
-  // ...
-  email: {
-    config: {
-      provider: 'mailgun',
-      providerOptions: {
-        key: env('MAILGUN_API_KEY'), // Required
-        domain: env('MAILGUN_DOMAIN'), // Required
-        url: env('MAILGUN_URL', 'https://api.mailgun.net'), //Optional. If domain region is Europe use 'https://api.eu.mailgun.net'
-      },
-      settings: {
-        defaultFrom: 'houssin.carnelian@gmail.com',
-        defaultReplyTo: 'houssin.carnelian@gmail.com',
-      },
-    },
-  },
-  // ...
-
+  
 });
